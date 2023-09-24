07506419e8a4c034f72c05baff8cf9188c77add3
Sintra <vicentepsalcedo@gmail.com>
2023-09-23 20:09:12 -0400

    made improvement to webscrapper

--- webscrapper/test.py
+++ webscrapper/test.py
@@ -2,11 +2,20 @@ import requests
 from bs4 import BeautifulSoup
  
 # Making a GET request
-r = requests.get('https://game8.co/games/Honkai-Star-Rail/archives/404256')
+url = 'https://game8.co/games/Honkai-Star-Rail/archives/404256'
+r = requests.get(url)
  
-soup = BeautifulSoup(r.content, 'html.parser')
+soup = BeautifulSoup(r.text, 'html.parser')
 
-s = soup.find('table', class_='a-table a-table a-table tablesorter')
-content = s.find_all('a')
+body = soup.find('body')
+col3 = body.find('div', class_= "l-3col")
+centerCol3 = col3.find('div', class_= "l-3colMain")
+mainCenterCol3 = centerCol3.find('div', class_= "l-3colMain__center l-3colMain__center--shadow")
+archiveStyleWrapper = mainCenterCol3.find('div', class_ = "archive-style-wrapper")
+# table = archiveStyleWrapper.find('table', class_= "a-table a-table a-table tablesorter")
+a = archiveStyleWrapper.find_all('a')
+count = 0
 
-print(content)
+for line in a:
+    count += 1
+    print(count, line.text)
